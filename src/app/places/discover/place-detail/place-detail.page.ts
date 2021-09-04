a3a8d87f1761f98428e712cf42aad4d8e23011f9
aamir12 <aamirkhan8878@gmail.com>
2021-09-04 19:12:15 +0530

    add pages

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ModalController, NavController } from '@ionic/angular';
 import { CreateBookingComponent } from 'src/app/bookings/create-booking/create-booking.component';
+import { Place } from '../../place.model';
+import { PlacesService } from '../../places.service';
 
 @Component({
   selector: 'app-place-detail',
@@ -10,22 +12,48 @@ import { CreateBookingComponent } from 'src/app/bookings/create-booking/create-b
 })
 export class PlaceDetailPage implements OnInit {
   constructor(
-    private router: Router,
+    private route: ActivatedRoute,
     private navCtrl: NavController,
-    private modalCtrl: ModalController
+    private modalCtrl: ModalController,
+    private placesService: PlacesService
   ) {}
 
-  ngOnInit() {}
+  place: Place;
+  ngOnInit() {
+    this.route.paramMap.subscribe((paramMap) => {
+      if (!paramMap.has('placeId')) {
+        this.navCtrl.navigateBack('/places/tabs/discover');
+        return;
+      }
+
+      this.place = this.placesService.getPlace(paramMap.get('placeId'));
+    });
+  }
 
   onBookPlace() {
     //this.router.navigateByUrl('/places/tabs/discover');
     //this.router.navigate(['/', 'places', 'tabs', 'discover']);
     //this.navCtrl.navigateBack('/places/tabs/discover'); //give animation effect
     //this.navCtrl.pop(); // it will not work when page is refresh
+
+    //In case mulitple modal give modal id at the time of open
     this.modalCtrl
-      .create({ component: CreateBookingComponent })
-      .then((modal) => {
-        modal.present();
+      .create({
+        component: CreateBookingComponent,
+        componentProps: {
+          selectedPlace: this.place,
+        },
+        //id:'modal-ID'
+      })
+      .then((modalEl) => {
+        modalEl.present();
+        return modalEl.onDidDismiss();
+      })
+      .then((resultData) => {
+        console.log(resultData.data, resultData.role);
+        if (resultData.role === 'confirm') {
+          console.log('BOOKED!');
+        }
       });
   }
 }
